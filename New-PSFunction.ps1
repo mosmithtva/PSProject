0202a3b86b192c7b53ce6544a36a20f0fe8e2410
Mike <mike@vnerd.io>
2017-12-18 14:01:05 -0500

    added psfunction script

---
+++ New-PSFunction.ps1
@@ -0,0 +1,83 @@
+<#
+    .SYNOPSIS
+	Creates a scaffold function
+
+    .DESCRIPTION
+	Creates a basic skeleton function
+
+    .EXAMPLE
+	$functions="Get-Thing","Set-Thing"
+	
+	foreach($item IN $functions)
+	{
+			New-PSFunction -Name $item -Path C:\PSModules\Public
+	}
+
+    .PARAMETER Name
+	REQUIRED
+	PSCredential
+
+	.PARAMETER Path
+	Option path to create the function. Default is PWD
+
+	.OUTPUTS
+	Create $Name.ps1 in specified path
+
+    .NOTES
+
+	.LINK
+	https://github.com/VNerdIO/New-PSProject
+#>
+Function New-PSFunction{
+	[CmdletBinding()]
+	Param([Parameter(Mandatory=$true)]
+		  [string]
+		  $Name,
+		  [string]
+		  $Path)
+	
+	begin{}
+	process{
+		try{
+
+			if(-not($Path)){ $Path = ".\" }
+
+$FunctionTemplate=@"
+<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .PARAMETER
+    
+    .OUTPUTS
+
+    .NOTES
+
+    .LINK
+#>
+Function $Name{
+	[CmdletBinding()]
+	Param([Parameter(Mandatory=`$true)]
+		  [PSCredential]
+		  `$Credential)
+	
+	begin{}
+	process{
+		try{}
+		catch{}
+		finally{}
+	}
+	end{}
+}
+"@
+
+			$FunctionTemplate | Out-File -Encoding Ascii "$Path\$name.ps1"
+		}
+		catch{}
+		finally{}
+	}
+	end{}
+}
